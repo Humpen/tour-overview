fd35da783b0585e74728b1b8b2edbecd03d78d5f
Nils Block <nils.block98@live.de>
2020-08-27 17:46:45 +0200

    Initial commit

---
+++ src/main/java/de/hsbhv/touroverview/views/about/AboutView.java
@@ -0,0 +1,20 @@
+package de.hsbhv.touroverview.views.about;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import de.hsbhv.touroverview.views.main.MainView;
+
+@Route(value = "about", layout = MainView.class)
+@PageTitle("About")
+@CssImport("./styles/views/about/about-view.css")
+public class AboutView extends Div {
+
+    public AboutView() {
+        setId("about-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
