9b6e364911fcce87b3962d0cdfff8f25025997b6
xdreamskillzxx <60485977+xdreamskillzxx@users.noreply.github.com>
2020-08-29 11:57:55 +0200

    Integration leaflet in vaadin 14

---
+++ src/main/java/de/hsbhv/touroverview/leaflet/MapLocationService.java
@@ -0,0 +1,49 @@
+package de.hsbhv.touroverview.leaflet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+/**
+ * Demo backend that accepts up to 100 fishing spots. Data is shared with all
+ * users.
+ */
+@Service
+@ApplicationScope
+public class MapLocationService {
+
+    private List<MapLocation> spots = new ArrayList<MapLocation>();
+
+    @PostConstruct
+    private void init() {
+
+        // Add some demo data
+
+        spots.add(new MapLocation(60.465071, 22.302923, "Halistenkoski"));
+        spots.add(new MapLocation(60.479928, 21.328347, "Kustavi"));
+        spots.add(new MapLocation(60.124169, 21.906335, "Kirjais"));
+    }
+
+    public List<MapLocation> getAll() {
+
+        return Collections.unmodifiableList(spots);
+    }
+
+    public void addSpot(MapLocation spot) {
+
+        // protect concurrent access since MapLocationService is a singleton
+        synchronized (spots) {
+
+            spots.add(spot);
+
+            if (spots.size() > 100) {
+                spots.remove(0);
+            }
+        }
+    }
+}
--- src/main/java/de/hsbhv/touroverview/views/tour/TourView.java
+++ src/main/java/de/hsbhv/touroverview/views/tour/TourView.java
@@ -8,7 +8,10 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import de.hsbhv.touroverview.leaflet.MapComponent;
+import de.hsbhv.touroverview.leaflet.MapLocationService;
 import de.hsbhv.touroverview.views.main.MainView;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route(value = "hello", layout = MainView.class)
@@ -19,17 +22,28 @@ public class TourView extends HorizontalLayout {
 
     private TextField name;
     private Button sayHello;
+    private MapComponent map;
+    private MapLocationService service;
+    @Autowired
+    public TourView(MapLocationService service) {
 
-    public TourView() {
+        this.service = service;
+        setSizeFull();
+        setPadding(false);
+        setSpacing(false);
+        map = new MapComponent();
+        map.setWidthFull();
+        map.addMarkersAndZoom(service.getAll());
+        add(map);
 
-        setId("hello-world-view");
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        add(name, sayHello);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-        sayHello.addClickListener( e-> {
-            Notification.show("Hello " + name.getValue());
-        });
+//        setId("hello-world-view");
+//        name = new TextField("Your name");
+//        sayHello = new Button("Say hello");
+//        add(name, sayHello);
+//        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+//        sayHello.addClickListener( e-> {
+//            Notification.show("Hello " + name.getValue());
+//        });
     }
 
 }
