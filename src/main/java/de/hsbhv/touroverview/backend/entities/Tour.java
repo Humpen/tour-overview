f4be310534518ce2a9c5ec9c3ed1fa16d928ba29
Nils <nils.block98@live.de>
2020-08-29 22:12:28 +0200

    old query shit with nodes

--- src/main/java/de/hsbhv/touroverview/backend/entities/Tour.java
+++ src/main/java/de/hsbhv/touroverview/backend/entities/Tour.java
@@ -2,9 +2,24 @@ package de.hsbhv.touroverview.backend.entities;
 
 import io.aexp.nodes.graphql.annotations.GraphQLProperty;
 
-@GraphQLProperty(name="tour")
+//@GraphQLProperty(name="tours", arguments = {@GraphQLArgument(name="name", value = "Tour 1")})
+@GraphQLProperty(name="tours")
 public class Tour {
-    String name;
-    int radius;
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+/*    public int radius;
+    public int getRadius() {
+        return radius;
+    }
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }*/
     //List<String> bilder;
 }
--- src/main/java/de/hsbhv/touroverview/backend/queries/TourQuery.java
+++ src/main/java/de/hsbhv/touroverview/backend/queries/TourQuery.java
@@ -1,9 +1,30 @@
 package de.hsbhv.touroverview.backend.queries;
 
 import de.hsbhv.touroverview.backend.entities.Tour;
+import io.aexp.nodes.graphql.GraphQLRequestEntity;
+import io.aexp.nodes.graphql.GraphQLResponseEntity;
+import io.aexp.nodes.graphql.GraphQLTemplate;
 
-public class TourQuery {
-    public static Tour getTourByName(String Name){
+import java.net.MalformedURLException;
+
+public class TourQuery implements QueryHandler{
+//    private Arguments arguments = new ArrayList<Argument>();
+
+    public TourQuery(){
+//        arguments.add(new Argument<String>("name", "Tour 1"));
+    }
+
+    public Tour getTourByName(String Name) throws MalformedURLException {
+        setAuthToken(standardToken);
+        GraphQLRequestEntity requestEntity = GraphQLRequestEntity.Builder()
+                .url("https://api-eu-central-1.graphcms.com/v2/ckbtfd1r500dl01z51rpf93bm/master")
+                .headers(headers)
+//                .arguments(Argume)
+                .request(Tour.class)
+                .build();
+        GraphQLTemplate graphQLTemplate = new GraphQLTemplate();
+        GraphQLResponseEntity<Tour> tour = graphQLTemplate.query(requestEntity, Tour.class);
+
         return null;
     }
 }
