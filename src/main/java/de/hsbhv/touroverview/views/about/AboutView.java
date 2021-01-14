ed011f0b059708f822850e59989f4f67866242ba
xdreamskillzxx <60485977+xdreamskillzxx@users.noreply.github.com>
2021-01-14 12:53:48 +0100

    text update and name update

--- src/main/java/de/hsbhv/touroverview/views/about/AboutView.java
+++ src/main/java/de/hsbhv/touroverview/views/about/AboutView.java
@@ -2,7 +2,7 @@ package de.hsbhv.touroverview.views.about;
 
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import de.hsbhv.touroverview.views.main.MainView;
@@ -14,9 +14,10 @@ public class AboutView extends Div {
 
     public AboutView() {
         setId("about-view");
-        add(new Label("Willkommen auf der Übersichtsseite für die AR-Touren APP! Über das Menü links kann eine die gewünschte Tour eingesehen werden."));
-
-
+        Span span = new Span();
+        String html = "Willkommen auf der Übersichtsseite für die AR-Touren APP! Über das Menü links kann eine die gewünschte Tour eingesehen werden.<br>" +
+                "Auf dieser Seite können nur Touren Daten angeschaut/gelesen werden. Sie können hier keine Daten zu einer bestehenden Tour hinzufügen. Dies geschieht über das Content Management System.<br>";
+        span.getElement().setProperty("innerHTML", html);
+        add(span);
     }
-
 }
